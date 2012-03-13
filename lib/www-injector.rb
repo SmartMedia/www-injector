2f06e936dec41c5d06b4c47e0d77a9a1b3771c86
Štefan Ľupták <stefan.luptak@smartmedia.cz>
2012-03-13 11:14:08 +0100

    Fixed bugs.

--- lib/www-injector.rb
+++ lib/www-injector.rb
@@ -8,16 +8,10 @@ class WwwInjector
   
   def call(env)
     if env['HTTP_HOST'] !~ /^www\./i
-      Rails.logger.info "\n\n -- WWW-INJECTOR REDIRECTING FROM " + env['HTTP_HOST'] + " TO " + 'www.' + Rack::Request.new(env).url
+      [301, { 'Location' => Rack::Request.new(env).url.sub(/^(https?\:\/\/)/i) {|s| $1 + 'www.'} }, ['Redirecting with www...']]
     else
-      Rails.logger.info "\n\n -- WWW-INJECTOR NOT REDIRECTING FROM " + env['HTTP_HOST']
-    end
-      
-      # [301, { 'Location' => Rack::Request.new(env).url.sub(/^(https?\:\/\/)/i) {|s| $1 + 'www.'} }, ['Redirecting with www...']]
-    # else
       @app.call(env)
-    # end
+    end
   end
   
 end
-
